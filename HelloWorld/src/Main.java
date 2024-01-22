b74f9f18f361e6ad9b57ee560bc7e85275e84b0c
Very-Ferrous <fatimconteh@gmail.com>
2024-01-22 14:10:47 +0000

    Fixed typo in line 3

--- HelloWorld/src/Main.java
+++ HelloWorld/src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        java.utli.Scanner scan = new java.util.Scanner(System.in);
+        java.util.Scanner scan = new java.util.Scanner(System.in);
         System.out.println("Hello, world!");
         System.out.println("What is your name?");
         String name = scan.nextLine();
