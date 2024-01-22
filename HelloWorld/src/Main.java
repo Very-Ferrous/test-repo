d0914ecf7ecb3cd604e1ffd30f031dfa1c420b4c
Very-Ferrous <fatimconteh@gmail.com>
2024-01-22 14:09:39 +0000

    Now it asks for the user's name and greets them

--- HelloWorld/src/Main.java
+++ HelloWorld/src/Main.java
@@ -1,5 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        java.utli.Scanner scan = new java.util.Scanner(System.in);
+        System.out.println("Hello, world!");
+        System.out.println("What is your name?");
+        String name = scan.nextLine();
+        System.out.println("Hello " + name + "!");
     }
 }
